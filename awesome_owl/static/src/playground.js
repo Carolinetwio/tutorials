b1c3b6a23d5b51c3ff1d517a47a8880a8f304bca
Carolinetwio <153074864+Carolinetwio@users.noreply.github.com>
2025-01-24 11:50:09 +0100

    [IMP] awesome_owl: Exercise 2 part 2: Separate counter component into counter directory

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,17 +1,8 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+import { Counter } from "./counter/counter"
 
-export class Counter extends Component {
-    static template = "awesome_owl.counter";  
-    setup() {
-        this.state = useState({ value: 1 });
-    }
-
-    increment() {
-        this.state.value++;
-    }
-}
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
