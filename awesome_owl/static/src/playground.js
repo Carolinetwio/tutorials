3d40059fa0dc74e47a3721a851a9b0945071f285
Carolinetwio <153074864+Carolinetwio@users.noreply.github.com>
2025-01-24 13:21:56 +0100

    [IMP] awesome_owl: Exercise 4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter"
 import { Card } from "./card/card"
 
@@ -9,4 +9,7 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
     
     static components = { Counter, Card }
+
+    cardtitle1 = "<div>some title 1</div>";
+    cardtitle2 = markup("<div>some title 2</div>");
 }
