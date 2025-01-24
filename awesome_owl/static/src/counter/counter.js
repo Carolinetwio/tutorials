a9bc1edb5e2e20282453fba9221d2ac614ea8511
Carolinetwio <153074864+Carolinetwio@users.noreply.github.com>
2025-01-24 14:27:59 +0100

    [IMP] awesome_owl: Exercise 6

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,6 +4,7 @@ import { Component, useState } from "@odoo/owl";
 
 
 export class Counter extends Component {
+    static props = {onChange: {type: Function, optional: true}};
     static template = "awesome_owl.counter";  
     setup() {
         this.state = useState({ value: 1 });
@@ -11,5 +12,6 @@ export class Counter extends Component {
 
     increment() {
         this.state.value++;
+        this.props.onChange();
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter"
 import { Card } from "./card/card"
 
@@ -9,6 +9,14 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
     
     static components = { Counter, Card }
+
+    setup() {
+        this.state = useState({ sum: 2 });
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 
     cardtitle1 = "<div>some title 1</div>";
     cardtitle2 = markup("<div>some title 2</div>");
