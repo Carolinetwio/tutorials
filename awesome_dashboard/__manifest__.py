ea60cdf6fbf84de802b22b67cd68bdedd8f97282
Carolinetwio <153074864+Carolinetwio@users.noreply.github.com>
2025-01-31 09:50:53 +0100

    [IMP] awesome_dashboard: Exercise 3 part 2: Add size as component props

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "Odoo",
     'website': "https://www.odoo.com/",
     'category': 'Tutorials/AwesomeDashboard',
-    'version': '0.5',
+    'version': '0.6',
     'application': True,
     'installable': True,
     'depends': ['base', 'web', 'mail', 'crm'],
--- awesome_dashboard/static/src/card/card.js
+++ awesome_dashboard/static/src/card/card.js
@@ -3,4 +3,7 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_dashboard.card";
+    static default_props = {
+        size: 1,
+    };
 }
