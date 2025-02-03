899da664933d071bff89117c0754282538d33dbe
Carolinetwio <153074864+Carolinetwio@users.noreply.github.com>
2025-02-03 13:59:06 +0100

    [IMP] awesome_dashboard: Exercise 7

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "Odoo",
     'website': "https://www.odoo.com/",
     'category': 'Tutorials/AwesomeDashboard',
-    'version': '0.10',
+    'version': '0.11',
     'application': True,
     'installable': True,
     'depends': ['base', 'web', 'mail', 'crm'],
--- awesome_dashboard/static/src/statistics_service.js
+++ awesome_dashboard/static/src/statistics_service.js
@@ -1,13 +1,18 @@
 /** @odoo-module */
 import { registry } from "@web/core/registry";
 import { rpc } from "@web/core/network/rpc";
-import { memoize } from "@web/core/utils/functions";
+import { reactive } from "@odoo/owl";
 
 const statisticsService = {
     start() {
-        return {
-            loadStatistics: memoize(() => rpc("/awesome_dashboard/statistics")),
-        };
+        const statistics = reactive({ isReady: false });
+        async function loadData() {
+            const updates = await rpc("/awesome_dashboard/statistics");
+            Object.assign(statistics, updates, { isReady: true });
+        }
+        setInterval(loadData, 10*1000);
+        loadData();
+        return statistics;
     },
 };
 registry.category("services").add("awesome_dashboard.statistics", statisticsService);
